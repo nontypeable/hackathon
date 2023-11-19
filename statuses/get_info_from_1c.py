5bdf6407291cfcb9567130c0a95c63503e62e3eb
Andrey Sadkov <ad.sadkov@gmail.com>
2023-11-19 12:00:08 +0300

    fix: remove non-existent values

--- statuses/get_info_from_1c.py
+++ statuses/get_info_from_1c.py
@@ -16,7 +16,7 @@ def get_qr_info_and_insert(url: str):
             if existing_record:
                 print("Record with this QR code already exists.")
             else:
-                execute("INSERT INTO products (qr_code) VALUES (?, ?)", (qr_code))
+                execute("INSERT INTO products (qr_code) VALUES (?)", (qr_code))
 
         return data
     else:
