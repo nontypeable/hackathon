722fb313ed723c264104d5beb9d6e423b449231e
Andrey Sadkov <ad.sadkov@gmail.com>
2023-11-19 11:54:41 +0300

    update: function for adding information about qr-codes to the database

--- statuses/get_info_from_1c.py
+++ statuses/get_info_from_1c.py
@@ -1,11 +1,23 @@
 import requests
 
+from db.db import *
 
-def get_qr_info(url: str, params: dict):
+
+def get_qr_info_and_insert(url: str, params: dict):
     response = requests.get(url, params=params)
 
     if response.status_code == 200:
         data = response.json()
+        qr_code = data.get("qr_code")
+
+        if qr_code:
+            # Проверяем, существует ли запись с таким QR-кодом в базе данных
+            existing_record = fetch_one("SELECT * FROM qr_data WHERE qr_code=?", (qr_code,))
+            if existing_record:
+                print("Record with this QR code already exists.")
+            else:
+                execute("INSERT INTO products (qr_code, other_data) VALUES (?, ?)", (qr_code, other_data))
+
         return data
     else:
         print("Failed to fetch data. Status code:", response.status_code)
